ce04cd0fe59659f351c87c1418e9bc79ccf02ae1
David Miller <david@deadpansincerity.com>
2013-07-02 15:18:10 +0100

    Add replyto example. refs #4

---
+++ examples/replyto.py
@@ -0,0 +1,18 @@
+import getpass, os
+
+fromaddr = raw_input('from > ')
+to  = raw_input('to > ')
+reply_to  = raw_input('CC > ')
+
+import letter
+
+class Message(letter.Letter):
+    Postie = letter.Postman() # Unauthorized SMTP, localhost:25
+
+    From    = fromaddr
+    To      = to
+    ReplyTo = reply_to
+    Subject = "I don't want to hear about it."
+    Body    = 'Hai, this is an Email! If you just hit reply in a sane mail program, it will go to {0}'.format(reply_to)
+
+Message.send()
